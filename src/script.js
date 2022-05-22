a833df0d2d608829b4fdfbc8d00b7bbe09eb7f18
Floaterest <floaterest@outlook.jp>
2022-05-22 11:33:22 -0400

    refactor(script.js) use jquery

--- src/script.js
+++ src/script.js
@@ -1,65 +1,15 @@
-const headers = ['h1', 'h2', 'h3'];
-const hidden = ' hidden';
-const toc = document.querySelector('#TOC');
-const isheader = tag => /h\d/.test(tag);
-
-function cmp(a, b) { // assume h1 > h2
-    // same header level
-    if (a === b) return 0;
-    // h1 > h2 => 1    h2 < h1 => -1
-    return (+a[1] < +b[1]) * 2 - 1;
-}
-
-function toggle(tag, element, hide) {
-    if (!element) return;
-
-    const t = element.tagName.toLowerCase();
-    if (isheader(t) && cmp(tag, t) <= 0) return;
-
-    if (hide) {
-        element.className += hidden;
-    } else {
-        element.className = element.className.replace(hidden, '');
-    }
-    toggle(tag, element.nextElementSibling, hide);
-}
-
-// open all details
-document.querySelectorAll('details:not(.example)').forEach(e => e.open = true);
+// open details that are not examples
+$('details:not(.example)').attr('open', true);
 
 // hide/show table of contents
-headers.forEach(
-    tag => document.querySelectorAll(tag + ':not(.title)').forEach(
-        element => element.onclick = () => toc.classList.toggle('show')
-    )
-);
+$('h1,h2,h3').click(() => $('#TOC').toggleClass('show'));
 
-// no colgroup
-// let deleted = false
-// const del = col => { col.remove(); deleted = true };
-// document.querySelectorAll('colgroup').forEach(del);
-// if (!col) {
-// 	console.log('No colgroup were found!');
-// }
-
-// click on header will hide all following elements until next header of same level
-// headers.forEach(
-// 	tag => document.querySelectorAll(tag + ':not(.title)').forEach(
-// 		element => element.onclick = ({target: { nextElementSibling } }) => {
-// 			toggle(tag, nextElementSibling, !nextElementSibling.className.includes('hidden'))
-// 		}
-// 	)
-// );
-// click on title to toggle all h1
-document.querySelector('h1.title').onclick = () => document.querySelectorAll('h1:not(.title)').forEach(h1 => h1.click());
-
-function accent(e) {
+// add pitch accent
+$('span[class^=a]').each((_, e) => {
     const acc = +e.className.substr(1);
     const text = e.innerText;
     if (acc !== 1) {
-        $('<span>').text(text[0]).addClass([
-            'low right'
-        ]).insertBefore(e);
+        $('<span>').text(text[0]).addClass('low right').insertBefore(e);
     }
 
     $('<span>').text(text.substring(acc !== 1, acc || undefined)).addClass([
@@ -67,10 +17,7 @@ function accent(e) {
     ]).insertBefore(e);
 
     if (acc && acc != text.length) {
-        $('<span>').addClass('low').text(
-            text.substring(acc)
-        ).insertBefore(e);
+        $('<span>').addClass('low').text(text.substring(acc)).insertBefore(e);
     }
     e.remove();
-}
-document.querySelectorAll('span[class^=a]').forEach(accent);
+});
