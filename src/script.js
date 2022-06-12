218eca54ead04d6bf9328ca739b5b21b499ae258
Floaterest <floaterest@outlook.jp>
2022-06-12 15:36:52 -0400

    feat(script) collapsable header

--- src/script.js
+++ src/script.js
@@ -1,8 +1,39 @@
-// open details that are not examples
-$('details:not(.example)').attr('open', true);
-
 // hide/show table of contents
-$('h1,h2,h3').click(() => $('#TOC').toggleClass('show'));
+$('html').click(() => $('#TOC').toggleClass('show'));
+$('body').click(e => e.stopPropagation());
+
+function cmp(ha, hb){
+    // compare header tags, h1 > h2
+    if(ha == hb) return 0;
+    // h1 > h2 => 1    h2 < h1 => -1
+    return (+ha[1] < +hb[1]) * 2 - 1;
+}
+
+function toggle({ target }){
+    const hidden = ' hidden';
+    const { tagName, nextElementSibling } = target;
+    const hide = !target.classList.contains('collapsed');
+    target.classList.toggle('collapsed');
+    const stack = [nextElementSibling];
+    // not using classList.add/remove to support multiple 'hidden's for nested hidden headers
+    const tog = e => hide
+        ? e.className += hidden
+        : e.className = e.className.replace(hidden, '');
+    while(stack.length){
+        const element = stack.pop();
+        const tag = element.tagName;
+        if(/h\d/i.test(tag) && cmp(tagName, tag) <= 0) continue;
+
+        tog(element);
+        stack.push(element.nextElementSibling);
+    }
+}
+
+// hide contents under a header
+$('h1:not(.title),h2,h3').click(toggle);
+
+// no colgroup
+$('colgroup').remove();
 
 // add pitch accent
 $('span[class^=a]').each((_, e) => {
@@ -12,12 +43,21 @@ $('span[class^=a]').each((_, e) => {
         $('<span>').text(text[0]).addClass('low right').insertBefore(e);
     }
 
-    $('<span>').text(text.substring(acc !== 1, acc || undefined)).addClass([
-        'high', acc && 'right'
-    ]).insertBefore(e);
+    // $('<span>').text(text.substring(acc !== 1, acc || undefined)).addClass([
+    //     'high', acc && 'right'
+    // ]).insertBefore(e);
+
+    $('<span>').text(
+        text.substring(acc !== 1, acc ? acc - 1 : undefined)
+    ).addClass('high').insertBefore(e);
+    
+    if(acc){
+        $('<span>').text(text[acc - 1]).addClass('high p right').insertBefore(e);
+    }
 
     if (acc && acc != text.length) {
         $('<span>').addClass('low').text(text.substring(acc)).insertBefore(e);
     }
     e.remove();
 });
+
