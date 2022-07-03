a7958de716202b3c9535d5d9a03e672993632288
Floaterest <floaterest@outlook.jp>
2022-07-03 15:57:38 -0400

    fix(texlive) install with profile

--- src/texlive.sh
+++ src/texlive.sh
@@ -3,4 +3,4 @@ tl="https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz"
 wget -q $tl
 tar -xf $(basename $tl)
 dir=$(ls -d install-tl-*/ | head -n1 | cut -f1 -d'/')
-sudo $dir/install-tl -q -profile $(dirname "$0")/texlive.profile
+sudo $dir/install-tl -q --profile $(dirname "$0")/texlive.profile
